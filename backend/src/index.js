e2af2f0227d8010c469d2171b753a0e6558cd3d4
Sayanchowdhury123 <sayanchoudhury506@gmail.com>
2026-03-05 12:03:23 +0530

    vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://collage-campus.vercel.app",
     methods: ["GET", "POST"],
   },
 });
